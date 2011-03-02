4b33f595b8bf476dc6371a33997df5262f712ae3
Warren Voelkl <warren_voelkl@hotmail.com>
2011-03-02 12:03:21 -0800

    removed the user file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,14 +5,22 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
+    //move these to be started when button pressed in gui
     TextServer * ts = new TextServer();
-    TextClient * tc = new TextClient(); //move these to be started when button pressed in gui
     textServer = new QThread(this);
-    textClient = new QThread(this);
+    textServer->start();
     connect(ts,SIGNAL(signalClientConnected(ClientConnect*)),
             this,SLOT(slotClientConnected(ClientConnect*)));
+    ts->moveToThread(textServer);
+
+    //move these to be started when button pressed in gui
+    TextClient * tc = new TextClient();
+    textClient = new QThread(this);
+    textClient->start();
     connect(tc,SIGNAL(signalTextRecieved(TextReceived*)),
             this,SLOT(slotTextRecieved(TextReceived*)));
+    tc->moveToThread(textClient);
 
     ui->setupUi(this);
 }
