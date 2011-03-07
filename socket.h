86e1533a1e396d696d36e859d17bc62c1e500c12
Warren Voelkl <warren_voelkl@hotmail.com>
2011-03-07 11:07:06 -0800

    added a tcpserver that uses select

--- socket.h
+++ socket.h
@@ -17,6 +17,7 @@ class Socket {
 public:
     Socket(int type, int port, int packetSize);
     int SetAsClient(const char * str);
+    int SetAsServer();
 
 
     int tx(const char * str, int length);
@@ -25,12 +26,17 @@ public:
 private:
     void createTCPSocket();
     void createUDPSocket();
+    int TCPServer();
+    int UDPServer();
+    int SetupSocket(const char * str);
+
     int buflen_;
     int socketType_;
     int sPort_;
     struct sockaddr_in client_, server_;
     int serverLength_;
     int socketDescriptor_;
+
 
 };
 
